7267fa70820c19284137c7af40e7f0d09109aed1
Mario Anticoli <mario@MacBook-Pro-di-Mario.local>
2017-11-23 13:22:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# nevw
+
+> starter pack electron+vuejs with DB support (nedb), webpack and buefy framework
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:8080 and start electron
+in one terminal:	yarn dev
+in another:			./node_modules/.bin/electron .
+
+# build for production with minification
+
+
+# build for production and view the bundle analyzer report
+
+```
+
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import {ipcRenderer} from 'electron'
+import Datastore from 'nedb'
+
+export default {
+  name: 'app',
+  components: {
+    Profile,
+    Page
+  },
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+  },
+  created: function () {
+    ipcRenderer.on('message-name', (event, res) => {
+    })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
